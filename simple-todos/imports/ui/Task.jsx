3d08dfa60ce323783c02a597a3df8431cfff1d30
Jeff Pinkston <jeff.pinkston@pearson.com>
2016-05-11 15:32:46 -0500

    Added user accounts and method security

--- simple-todos/imports/ui/Task.jsx
+++ simple-todos/imports/ui/Task.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import { Meteor } from 'meteor/meteor';
 
 import { Tasks } from '../api/tasks.js';
 
@@ -6,13 +7,11 @@ import { Tasks } from '../api/tasks.js';
 export default class Task extends Component {
   toggleChecked() {
     //Set the checked property to teh opposite of its current value
-    Tasks.update(this.props.task._id, {
-      $set: {checked: !this.props.task.checked }
-    });
+    Meteor.call('tasks.setChecked', this.props.task._id, !this.props.task.checked);
   }
 
   deleteThisTask(){
-      Tasks.remove(this.props.task._id);
+    Meteor.call('tasks.remove', this.props.task._id);
   }
 
   render() {
@@ -33,7 +32,9 @@ export default class Task extends Component {
           onClick={this.toggleChecked.bind(this)}
           />
 
-        <span className="text">{this.props.task.text}</span>
+        <span className="text">
+          <strong>{this.props.task.username}</strong>: {this.props.task.text}
+        </span>
       </li>
     );
   }
